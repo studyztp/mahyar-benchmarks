389b6e66dd6243d2cc01997324ce1456c1c1f6a6
Mahyar Samani <msamani@ucdavis.edu>
2023-11-30 02:58:25 -0800

    Initial commit
    This commit adds the source code for NPB as well as annotating it
    with hooks for annotations of ROI. It also adds a standard
    interface for annotation with gem5 and papi.

---
+++ roi/include/roi.h
@@ -0,0 +1,14 @@
+#ifndef __COMMON_ROI_H__
+#define __COMMON_ROI_H__
+
+void annotate_init_();
+
+void region_begin_(const char* region);
+void region_end_(const char* region);
+
+void roi_begin_();
+void roi_end_();
+
+void thread_init_();
+
+#endif // __COMMON_ROI_H__
---
+++ roi/roi.c
@@ -0,0 +1,98 @@
+#include "include/roi.h"
+
+#if defined(PAPI)
+#include <stdio.h>
+#include <pthread.h>
+#include "papi.h"
+
+void annotate_init_()
+{
+    int retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT) {
+        printf("PAPI_library_init failed due to %d.\n", retval);
+    }
+}
+
+void region_begin_(const char* region)
+{
+    int retval = PAPI_hl_region_begin(region);
+    if (retval != PAPI_OK) {
+        printf("PAPI_hl_region_begin failed due to %d.\n", retval);
+    }
+}
+
+void region_end_(const char* region)
+{
+    int retval = PAPI_hl_region_end(region);
+    if (retval != PAPI_OK) {
+        printf("PAPI_hl_region_end failed due to %d.\n", retval);
+    }
+}
+
+void roi_begin_()
+{
+    region_begin_("roi");
+}
+
+void roi_end_()
+{
+    region_end_("roi");
+}
+
+void thread_init_()
+{
+    PAPI_thread_init(pthread_self);
+}
+
+#elif defined(GEM5)
+#include <stdio.h>
+#include "gem5/m5ops.h"
+
+void annotate_init_()
+{}
+
+void region_begin_(const char* region)
+{}
+
+void region_end_(const char* region)
+{}
+
+void roi_begin_()
+{
+    m5_work_begin(0,0);
+}
+
+void roi_end_()
+{
+    m5_work_end(0,0);
+}
+
+void thread_init_()
+{}
+
+#else
+#include <stdio.h>
+
+void annotate_init_()
+{}
+
+void region_begin_(const char* region)
+{}
+
+void region_end_(const char* region)
+{}
+
+void roi_begin_(const char* region)
+{
+    printf("Reached the beginning of %s region.\n", region);
+}
+
+void roi_end_(const char* region)
+{
+    printf("Reached the end of %s region.\n", region);
+}
+
+void thread_init_()
+{}
+
+#endif // defined(PAPI)
